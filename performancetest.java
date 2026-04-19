bd39855d425bd5075664e26a1a945284cde3313f
Soujanya Suresh Harikantra <harikantrasoujanya197@gmail.com>
2026-04-19 21:05:50 +0100

    Update performancetest.java

--- performancetest.java
+++ performancetest.java
@@ -1,3 +1,9 @@
+
+//2.b. Develop a Java program to make a Performance Test on StringBuffer and StringBuilder for appending
+//the string ‘AIET’ for 10000 times both in the StringBuffer and StringBuilder. 
+//Justify your answer whichone is better.
+
+
 package adjava;
 
 
